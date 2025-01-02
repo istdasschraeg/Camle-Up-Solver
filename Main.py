1473784637fb7ac19f55f1984cd67efb031455b9
Admurai <89091358+Admurai@users.noreply.github.com>
2025-01-02 17:17:00 +0100

    Outline

---
+++ Main.py
@@ -0,0 +1,16 @@
+
+
+# Soll wahrscheinlichkeit von würfeln aus der Pyramide erechen 
+
+#soll den besten Zug finden
+
+#braucht Input für die würfel
+
+import random
+class dice:
+    wurf= 0
+    farbe= ""
+    
+
+    def roll():
+        random.int 
