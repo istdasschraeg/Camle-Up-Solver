455948fd0573c5b7f4c363450a989511d5607dc9
Admurai <89091358+Admurai@users.noreply.github.com>
2025-01-03 12:52:13 +0100

    doesnt work but also doesnt make sense

--- Main.py
+++ Main.py
@@ -21,7 +21,7 @@ class dice:
         pass
 
     def roll(self):
-        self.wurf = random.randint(1-6) 
+        self.wurf = random.randint(1,3) 
         print (self.wurf)
 
 class camel:
@@ -56,20 +56,40 @@ class pyramide:
         self.Not_pyramide =[]
         pass
     def move(self,camel,moves):
-        new_field=camel.field + int(moves) 
-        for camle in self.camle_list:
-            if camle.field == new_field:
-                camel.position +=1
 
-        camel.field = new_field
+        if not camel.position== 0:
+                new_poition=0
+                new_field=0
+                new_field=camel.field + int(moves) 
+                camels_on_field = [c for c in self.camle_list if c.field == new_field]
+                camels_on_field.sort(key=lambda camel: camel.position)
+                for camle in camels_on_field:
+                    camels_on_field_new = [c for c in self.camle_list if c.field == new_field]
+                    print(len(camels_on_field_new))
+                    new_poition=len(camels_on_field_new) 
+                    print (new_field)
+                    camle.field = new_field
+                    camle.position= new_poition
+        
+        else:
+            new_poition=0
+            new_field=0
+            new_field=camel.field + int(moves) 
+            camels_on_field = [c for c in self.camle_list if c.field == new_field]
+            print(len(camels_on_field))
+            new_poition=len(camels_on_field) 
+            print (new_field)
+            camel.field = new_field
+            camel.position= new_poition  
     
     def produce_dice(self):
         current_dice  = random.choice(self.dice_list)
-        
+        current_dice1 =current_dice
+        self.dice_list.remove(current_dice)
         self.Not_pyramide.append(current_dice)
-        return current_dice.colour
+        return current_dice1.colour
 
-    def take_initial_input (self):
+    def take_initial_input (self):  
         for camel in self.camle_list:
             camel_color=input(f"What camle was rolled?") 
             roll= input("Roll:")
@@ -91,9 +111,6 @@ class pyramide:
     def give_all_poistions(self):
         for camel in self.camle_list:
             print(camel.name,"Position:",camel.position, "Color:",camel.color, "Field:", camel.field)            
-           
-    def change_field (self,camel_color,field):
-        self.take_color_return_object(camel_color).field=field  #does not work with  position ignores it 
     
     def save_current_position(self):
         file1 = open("save.txt","r+")
@@ -104,7 +121,7 @@ class pyramide:
 
     def load_safe(self):
             
-        file1 = open("save.txt","r+") 
+        file1 = open("save.txt","r+")
         save=file1.readlines()
         self.camle_list=[]
         del self.c1 ,self.c2, self.c3, self.c4, self.c5
@@ -114,25 +131,106 @@ class pyramide:
         self.c4= camel (save[9].replace("\n",""),int(save[10]),int(save[11])) 
         self.c5= camel (save[12].replace("\n",""),int(save[13]),int(save[14])) 
         self.camle_list= [self.c1, self.c2, self.c3, self.c4, self.c5]
-        
+         
             
 
     def run(self):
 
-        for i in 5 :
-            self.produce_dice()
-            wurf = random.randint(1-6) 
-            self.move(self.take_color_return_object(self.current_dice.colour()),wurf)
+        for dice in self.dice_list :
+            print ("lol")
+            wurf = random.randint(1,3)
+            print(wurf) 
+            print(self.produce_dice())
+            self.move(self.take_color_return_object(self.produce_dice()),wurf)
+        self.dice_list = [self.dred, self.dgreen, self.dyellow, self.dblack, self.dwhite] 
         self.give_all_poistions()
+
+    def calculate_probability(self):
+        times=10
+        red_1 =0;red_2 =0; red_3 =0;red_4 =0;red_5 =0
+        green_1= 0;green_2= 0;green_3= 0;green_4= 0;green_5= 0
+        yellow_1=0;yellow_2=0;yellow_3=0;yellow_4=0;yellow_5=0
+        white_1=0;white_2=0;white_3=0;white_4=0;white_5=0
+        black_1=0; black_2=0; black_3=0; black_4=0; black_5=0
+
+        for i in range(times):
+            self.run()
+            self.sort_for_best()
+            if self.camle_list[0].color == "red":
+                red_1+=1
+            elif self.camle_list[0].color == "yellow":
+                yellow_1+=1
+            elif self.camle_list[0].color == "green":
+                green_1+=1
+            elif self.camle_list[0].color == "white":
+                white_1+=1
+            elif self.camle_list[0].color == "black":
+                black_1+=1
+            if self.camle_list[1].color == "red":
+                red_2+=1
+            elif self.camle_list[1].color == "yellow":
+                yellow_2+=1
+            elif self.camle_list[1].color == "green":
+                green_2+=1
+            elif self.camle_list[1].color == "white":
+                white_2+=1
+            elif self.camle_list[1].color == "black":
+                black_2+=1
+            if self.camle_list[2].color == "red":
+                red_3+=1
+            elif self.camle_list[2].color == "yellow":
+                yellow_3+=1
+            elif self.camle_list[2].color == "green":
+                green_3+=1
+            elif self.camle_list[2].color == "white":
+                white_3+=1
+            elif self.camle_list[2].color == "black":
+                black_3+=1
+            if self.camle_list[3].color == "red":
+                red_4+=1
+            elif self.camle_list[3].color == "yellow":
+                yellow_4+=1
+            elif self.camle_list[3].color == "green":
+                green_4+=1
+            elif self.camle_list[3].color == "white":
+                white_4+=1
+            elif self.camle_list[3].color == "black":
+                black_4+=1
+            if self.camle_list[4].color == "red":
+                red_5+=1
+            elif self.camle_list[4].color == "yellow":
+                yellow_5+=1
+            elif self.camle_list[4].color == "green":
+                green_5+=1
+            elif self.camle_list[4].color == "white":
+                white_5+=1
+            elif self.camle_list[4].color == "black":
+                black_5+=1
+
+            self.load_safe()
+
+            
+            times +=1
+
+        print()
+
+
+
+    def sort_for_best(self):
+        self.camle_list.sort(key=lambda camel: (-camel.field, camel.position)) 
+        
+    
+    
 
 
 game =pyramide()
 
-game.load_safe()
 #game.take_initial_input()
-game.give_all_poistions()
-game.save_current_position()
-
-
+game.load_safe()
+#game.give_all_poistions()
+#game.save_current_position()
+game.run()
+game.sort_for_best()
+#game.calculate_probability()
 
 
